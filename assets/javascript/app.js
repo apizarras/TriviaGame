d63ea50baac92e747035b8243c4ac0751c5deeb5
apizarras <angelalee.p@gmail.com>
2019-03-23 20:22:49 -0500

    psuedo code js

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+$(document).ready();
+
+//start trivia game
+
+//dynamic questions to display & correct answer for each question
+const questionAnswer = [
+    {question: "Which Bruce Lee movie also stared Chuck Norris?",
+    possibleAnswers: ["I am Bruce Lee", "The Way of the Dragon", "Fist of Fury", " Enter the Dragon"]
+    correctAnswer: "The Way of the Dragon"
+    },
+    {
+    question: "Which Bruce Lee movie also stared Kareem Abdul-Jabbar?",
+    possibleAnswers: ["The Big Boss", "Ironside", "Game of Death", "The Green Hornet"],
+    correctAnswer: "Game of Death"
+    }
+    {
+    question: "What martial arts movie has won an Oscar?"
+    possibleAnswers: ["Crouching Tiger, Hidden Dragon", "Ip Man", "Kill Bill Vol. 1", "Kung Fu Panda"]
+    correctAnswer: "Crouching Tiger, Hidden Dragon"
+    }
+]
+let correct = 0;
+//loop through each quesetion from the array of questions
+
+//display the question and possible answers with radio button
+//start timer for that question
+//onclick determine if the user selected the correct answer && stop timer
+//display whether the player got the question right or wrong
+//if correct then add to the count of correct answers && call pause functin && display next question
+
+//function for pause before displaying the next question
+window.setTimeout(out-your-function-here, milliseconds-here)
+
+
+//button with function to restart the game
+    //include reset of all variables
